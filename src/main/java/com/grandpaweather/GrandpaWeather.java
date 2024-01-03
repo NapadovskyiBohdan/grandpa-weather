2a2a1a67c633c15d9096d3d55a201d7889efafd5
bohdan.napadovskyi <napadovskibohdan@gmail.com>
2024-01-03 14:51:01 +0200

    make some changes with deploy process

--- src/main/java/com/grandpaweather/GrandpaWeather.java
+++ src/main/java/com/grandpaweather/GrandpaWeather.java
@@ -5,10 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@SpringBootApplication
 @EnableMongoRepositories
 @EnableMongock
 public class GrandpaWeather {
